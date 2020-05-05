65f1822b993f0457fad3e19f117a4a0d903a7ab1
Gittenburg <gittenburg@fsinf.at>
2020-05-05 11:00:12 +0200

    remove unused course definitions

--- cleanup.py
+++ cleanup.py
@@ -16,5 +16,10 @@ def sort_group(group):
 
 sort_group(curriculum.find('group'))
 
+crefs = set([c.get('name') for c in curriculum.findall('.//cref')])
+for course in curriculum.find('courses'):
+	if course.get('name') not in crefs:
+		course.getparent().remove(course)
+
 with open(sys.argv[1], 'w') as f:
 	f.write(lxml.etree.tostring(curriculum, encoding='unicode') + '\n')
